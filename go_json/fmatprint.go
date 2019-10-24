7151c5a8e41d9356c1b993ee111b6cc25fe1805b
dabai <yang_youwei81@163.com>
2019-10-24 15:50:47 +0800

    Create fmatprint.go

---
+++ go_json/fmatprint.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
+type Line struct {
+	Ipaddr  string `json:"ip_port"`
+	Comment string `json:"comment"`
+}
+
+type Lines struct {
+	Lines []Line `json:"lines"`
+}
+
+func main()  {
+	var l Lines
+	l.Lines = append(l.Lines,Line{Ipaddr:"192.168.2.100：443",Comment:"美国"})
+	l.Lines = append(l.Lines,Line{Ipaddr:"192.168.2.100：443",Comment:"美国"})
+	b,err := json.Marshal(l)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var str bytes.Buffer
+	_ = json.Indent(&str, []byte(b), "", "	")
+	fmt.Println(str.String())
+}
