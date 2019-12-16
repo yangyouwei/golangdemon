5d088d04788bae4f1dc2f16c27d604cdd6b220e8
dabai <yang_youwei81@163.com>
2019-12-16 14:38:05 +0800

    Create main.go

---
+++ jsonstrtojson/main.go
@@ -0,0 +1,71 @@
+{
+	"up": true,
+	"pending": false,
+	"available": true,
+	"autostart": true,
+	"dynamic": false,
+	"uptime": 110308,
+	"l3_device": "eth0.1",
+	"proto": "dhcp",
+	"device": "eth0.1",
+	"metric": 0,
+	"dns_metric": 0,
+	"delegation": true,
+	"ipv4-address": [
+		{
+			"address": "192.168.2.162",
+			"mask": 24
+		}
+	],
+	"ipv6-address": [
+		
+	],
+	"ipv6-prefix": [
+		
+	],
+	"ipv6-prefix-assignment": [
+		
+	],
+	"route": [
+		{
+			"target": "192.168.2.1",
+			"mask": 32,
+			"nexthop": "0.0.0.0",
+			"source": "192.168.2.162\/32"
+		},
+		{
+			"target": "0.0.0.0",
+			"mask": 0,
+			"nexthop": "192.168.2.1",
+			"source": "192.168.2.162\/32"
+		}
+	],
+	"dns-server": [
+		"223.5.5.5",
+		"202.99.160.68",
+		"8.8.8.8"
+	],
+	"dns-search": [
+		
+	],
+	"inactive": {
+		"ipv4-address": [
+			
+		],
+		"ipv6-address": [
+			
+		],
+		"route": [
+			
+		],
+		"dns-server": [
+			
+		],
+		"dns-search": [
+			
+		]
+	},
+	"data": {
+		"leasetime": 3600
+	}
+}
