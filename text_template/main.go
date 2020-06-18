04d96c14ece768e4987be253897d0db85a2502ec
dabai <yang_youwei81@163.com>
2020-06-18 09:14:36 +0800

    Update main.go

--- text_template/main.go
+++ text_template/main.go
@@ -18,8 +18,7 @@ type Services struct {
 }
 
 
-const templateText =`{{.}}
-upstream {{.Name}} {
+const templateText =`upstream {{.Name}} {
 	zone upstream-{{.Name}} 64k;{{range .Services}}
 	server {{.Address}}:{{.Port}} max_fails=3 fail_timeout=60 weight=1;{{else}}server 127.0.0.1:65535; # force a 502{{end}}
 	ip_hash;
